432b72e350f7bd8ebe31789da8697471f52a7b55
EmilioC1 <ec2736@princeton.edu>
2024-08-14 14:26:58 -0400

    added more things

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ vim.g.mapleader = " "
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
 if not vim.uv.fs_stat(lazypath) then
   local repo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
@@ -12,7 +11,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local lazy_config = require "configs.lazy"
-
 -- load plugins
 require("lazy").setup({
   {
@@ -34,4 +32,5 @@ require "nvchad.autocmds"
 
 vim.schedule(function()
   require "mappings"
+  require "set"
 end)
